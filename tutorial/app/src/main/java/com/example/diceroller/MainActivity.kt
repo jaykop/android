28a6fd88d04aea42f98531a08cd869c0aea9a317
jaykop <jaykop.jy@gmail.com>
2019-12-10 19:02:10 -0800

    first commit

--- tutorial/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ tutorial/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tutorial
+package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- tutorial/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ tutorial/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.tutorial
+package com.example.diceroller
 
 import org.junit.Test
 
